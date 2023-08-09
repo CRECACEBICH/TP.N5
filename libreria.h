ba148c1e2b0e7bd5b0a4adc3612cdc470f30b307
CRECACEBICH <141262181+CRECACEBICH@users.noreply.github.com>
2023-08-09 17:19:24 -0300

    Update libreria.h

--- libreria.h
+++ libreria.h
@@ -1,34 +1,17 @@
 #ifndef lcd_h
 #define lcd_h
 
-#include <stdint.h>
-#include "MKL25Z4.h"
+#define registro_pin 0
+#define readwrite_pin 1
+#define habilitacion_pin 2
+#define datos_pin 4
 
-#define LCD_LINE_ADDRESS 0x27
+void lcd_inicio();
+void lcd_borrar();
+void lcd_comienzo();
+void lcd_estcursor(int fil, int col);
+void lcd_escribircaracter(char caracter);
+void lcd_escribiroracion(const char oracion);
+void lcd_crearcaracter(int posicion, const char posicionlcd);
 
-#define PORT_PCR_ODE_MASK (1u << 5)
-
-#define LCD_COMMAND_LIMPIAR_DISPLAY 0x01
-#define LCD_COMMAND_RETURN_HOME 0x02
-#define LCD_COMMAND_DISPLAY_ON 0x0C
-#define LCD_COMMAND_DISPLAY_OFF 0x08
-#define LCD_COMMAND_CURSOR_ON 0x0E
-#define LCD_COMMAND_CURSOR_OFF 0x0C
-#define LCD_COMMAND_BLINK_ON 0x0F
-#define LCD_COMMAND_BLINK_OFF 0x0C
-
-#define LCD_NUM_FILAS 4
-#define LCD_NUM_COLUMNAS 20
-
-const uint8_t LCD_LINE_ADDRESS[LCD_NUM_FILAS];
-#define LCD_SET_DD_RAM_ADDRESS 0x80
-
-void lcd_inicio(void);
-
-void lcd_imprimir(const char* text);
-
-void lcd_limpiar();
-
-void lcd_establecercursor(uint8_t fila, uint8_t columna);
-
-#endif  
+#endif
