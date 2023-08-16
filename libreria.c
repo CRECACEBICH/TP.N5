eb86a1c372319052b8f1910c4e73d380305245bf
CRECACEBICH <141262181+CRECACEBICH@users.noreply.github.com>
2023-08-16 16:50:13 -0300

    Update libreria.c

--- libreria.c
+++ libreria.c
@@ -1,44 +1,41 @@
-#include "lcd_bccc.h"
+#include "lcd.h"
 #include "delay.h"
 
+void lcd_comando(){
+    data_pin= cmd;
+    digitalWrite(rs_pin, LOW);
+    digitalWrite(en_pin, HIGH);
+    delay_us(1);
+    digitalWrite(en_pin, LOW);
+    delay_us(100);
+}
+
+void lcd_escribir(int data){
+    data_pin= data;
+    digitalWrite(rs_pin, HIGH);
+    digitalWrite(en_pin, HIGH);
+    delay_us(1);
+    digitalWrite(en_pin, LOW);
+    delay_us(100);
+}
+
 void lcd_inicio(){
-    if(){
-        
-    }
+    DDRB = 0xFF;
+    pinMode(rs_pin, OUTPUT);
+    pinMode(en_pin, OUTPUT);
+
+    delay_ms(20);
+
+    lcd_comando(0x38);
+    lcd_comando(0x0E);
+    lcd_comando(0x01);
+    lcd_comando(0x06);
 }
 
-void lcd_borrar(){
-    if(){
-
-    }
-}
-
-void lcd_comienzo(){
-    if(){
-
-    }
-}
-
-void lcd_estcursor(int fil, int col){
-    if(){
-
-    }
-}
-
-void lcd_escribircaracter(char caracter){
-    if(){
-
-    }
-}
-
-void lcd_escribiroracion(const char oracion){
-    if(){
-
-    }
-}
-
-void lcd_crearcaracter(int posicion, const char posicionlcd){
-    if(){
-
+void lcd_imprimir(){
+    void lcd_print(const char texto){
+        while (texto){
+        lcd_escribir(texto++);
+        }
     }
 }
