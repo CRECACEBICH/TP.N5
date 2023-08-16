d20894441949cdcf31950adf8f68bfcc8c2cba11
CRECACEBICH <141262181+CRECACEBICH@users.noreply.github.com>
2023-08-16 16:50:34 -0300

    Update libreria.h

--- libreria.h
+++ libreria.h
@@ -1,17 +1,14 @@
 #ifndef lcd_h
 #define lcd_h
+#define en_pin 1
+#define rs_pin 2
+#define data_pin PORTB
 
-#define registro_pin 0
-#define readwrite_pin 1
-#define habilitacion_pin 2
-#define datos_pin 4
-
+void lcd_comando();
 void lcd_inicio();
 void lcd_borrar();
 void lcd_comienzo();
-void lcd_estcursor(int fil, int col);
-void lcd_escribircaracter(char caracter);
-void lcd_escribiroracion(const char oracion);
-void lcd_crearcaracter(int posicion, const char posicionlcd);
+void lcd_escribir();
+void imprimir();
 
 #endif
